ec28cb872c01f5e9e9c87cc9454d8bf6580ab352
Zar1pova <109104483+Zar1pova@users.noreply.github.com>
2022-10-03 14:51:35 +0300

    Update Task01PS1.java

--- problem-set-1/Task01PS1.java
+++ problem-set-1/Task01PS1.java
@@ -5,7 +5,7 @@
 */
 import java.util.Scanner;
 
-public class Main {
+public class Task01PS1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
